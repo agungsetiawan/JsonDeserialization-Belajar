00c668c4e2a1a07f863b55c9441d49337ce7d73f
agung_setiawan <ag.setiawan@id.jmtech.co.jp>
2014-06-09 15:08:59 +0700

    First Commit, All is well

---
+++ JsonDeserialization-Belajar/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDeserialization_Belajar
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name="email")]
+        public string Email { get; set; }
+
+        [DataMember(Name="job")]
+        public string Job { get; set; }
+    }
+}
---
+++ JsonDeserialization-Belajar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDeserialization_Belajar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = WebRequest.Create("http://echo.jsontest.com/name/AgungSetiawan/email/com.agungsetiawan@gmail.com/job/SoftwareEngineer");
+            var response = request.GetResponse();
+
+            Person person = null;
+
+            using (var stream = response.GetResponseStream())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(Person));
+                person = (Person)serializer.ReadObject(stream);
+            }
+
+            Console.WriteLine("{0}, {1}, {2}",person.Name,person.Email,person.Job);
+
+
+
+            Person p = new Person();
+            p.Name = "A Name";
+            p.Email = "An Email";
+            p.Job = "A Job";
+
+            var memoryStream = new MemoryStream();
+            var ser = new DataContractJsonSerializer(typeof(Person));
+
+            ser.WriteObject(memoryStream, p);
+            memoryStream.Position = 0;
+
+            StreamReader streamReader = new StreamReader(memoryStream);
+            Console.WriteLine(streamReader.ReadToEnd());
+
+            Console.ReadLine();
+        }
+    }
+}
